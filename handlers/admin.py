7f9d978439b6392782142befbe31b0289d98ddc0
andredance <lifeapricot@gmail.com>
2022-06-23 23:14:45 +0300

    new files

--- handlers/admin.py
+++ handlers/admin.py
@@ -26,66 +26,72 @@ async def make_changes_command(message: types.Message):
 # Начало диалога загрузки нового пункта меню
 # @dp.message_handler(commands='Загрузить', state=None)
 async def cm_start(message: types.Message):
-    await FSMAdmin.photo.set()
-    await message.reply('Загрузи фото')
-
-
-# Ловим первый ответ и пишем в словарь
-# @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
-async def load_photo(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['photo'] = message.photo[0].file_id
-    await FSMAdmin.next()
-    await message.reply("Теперь введи название")
-
-
-# Ловим второй ответ
-# @dp.message_handler(state=FSMAdmin.name)
-async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text
-    await FSMAdmin.next()
-    await message.reply("Введи описание")
-
-
-# Ловим третий оввет
-# @dp.message_handler(state=FSMAdmin.description)
-async def load_description(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['description'] = message.text
-    await FSMAdmin.next()
-    await message.reply("Теперь укажи цену")
-
-
-# Ловим последний ответ и используем полученные данные
-# @dp.message_handler(state=FSMAdmin.price)
-async def load_price(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['price'] = float(message.text)
-
-    async with state.proxy() as data:
-        await message.reply(str(data))
-    await state.finish()
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set()
+        await message.reply('Загрузи фото')
 
 
 # Выход из состояний
 # @dp.message_handler(state="*", commands='отмена')
 # @dp.message_handler(Text(equaLs='отмена', ignore_case=True), state="*")
-async def cancel_handler(messange: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await messange.reply('OK')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply('OK')
+
+
+# Ловим первый ответ и пишем в словарь
+# @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
+async def load_photo(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+        await FSMAdmin.next()
+        await message.reply("Теперь введи название")
+
+
+# Ловим второй ответ
+# @dp.message_handler(state=FSMAdmin.name)
+async def load_name(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await FSMAdmin.next()
+        await message.reply("Введи описание")
+
+
+# Ловим третий оввет
+# @dp.message_handler(state=FSMAdmin.description)
+async def load_description(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['description'] = message.text
+        await FSMAdmin.next()
+        await message.reply("Теперь укажи цену")
+
+
+# Ловим последний ответ и используем полученные данные
+# @dp.message_handler(state=FSMAdmin.price)
+async def load_price(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = float(message.text)
+
+        async with state.proxy() as data:
+            await message.reply(str(data))
+        await state.finish()
 
 
 # Регистрируем хендлеры
 def register_handlers_admin(dp: Dispatcher):
-    dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
     dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
+    dp.register_message_handler(cancel_handler, state="*", commands='отмена')
+    dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state="*")
     dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
-    dp.register_message_handler(cancel_handler, state="*", commands='отмена')
-    dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state="*")
+    dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
